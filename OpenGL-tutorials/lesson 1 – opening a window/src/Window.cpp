9cc9d07013d88f36625ac1f84585d8e3ecedd669
Valeryi Savich <Relrin78@gmail.com>
2017-02-07 22:26:55 +0300

    Refactoring existing codebase for lesson 1

---
+++ OpenGL-tutorials/lesson 1 – opening a window/src/Window.cpp
@@ -0,0 +1,58 @@
+#include "Window.h"
+#include <iostream>
+
+#include "Input.h"
+
+Window::Window(int width, int height, const std::string name)
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Error initializing in GLFWn" << std::endl;
+        return;
+    }
+
+    // Use OpenGL 4.1 without forward-compatibility.
+    glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);  // Use FSAA x4.
+
+    window = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Can't create the GLFW window. " << std::endl;
+        glfwTerminate();
+        return;
+    }
+}
+
+
+// Initialize OpenGL context.
+void Window::Initialize()
+{
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW.
+    glewExperimental = GL_TRUE;  // Required for core profile in OpenGL.
+    GLenum result = glewInit();
+    if (result != GLEW_OK)
+    {
+        std::cerr << "Error initializing in GLEWn" << std::endl;
+        std::cerr << glewGetErrorString(result) << std::endl;
+        return;
+    }
+}
+
+
+void Window::Run()
+{
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    while (Input::IsKeyPressed(window, KEYBOARD_KEY::ESC) && glfwWindowShouldClose(window) == 0);
+}
