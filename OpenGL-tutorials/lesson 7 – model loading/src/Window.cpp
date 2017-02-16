c95e73f0c3c3566d4cea3c0219396a6f22552054
Valeryi Savich <Relrin78@gmail.com>
2017-02-16 19:51:38 +0300

    Added solution to the Lesson 7

---
+++ OpenGL-tutorials/lesson 7 – model loading/src/Window.cpp
@@ -0,0 +1,164 @@
+#include "Window.h"
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Input.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "Controls.h"
+#include "ObjLoader.h"
+
+static const int TRIANGLE_VERTICES = 3;
+static const int CUBE_VERTICES = 12 * TRIANGLE_VERTICES;
+
+
+Window::Window(int width, int height, const std::string name)
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Error initializing in GLFWn" << std::endl;
+        return;
+    }
+
+    // Use OpenGL 4.1 without forward-compatibility.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);  // Use FSAA x4.
+
+    window = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Can't create the GLFW window. " << std::endl;
+        glfwTerminate();
+        return;
+    }
+}
+
+
+// Initialize OpenGL context.
+void Window::Initialize()
+{
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW.
+    glewExperimental = GL_TRUE;  // Required for core profile in OpenGL.
+    GLenum result = glewInit();
+    if (result != GLEW_OK)
+    {
+        std::cerr << "Error initializing in GLEWn" << std::endl;
+        std::cerr << glewGetErrorString(result) << std::endl;
+        return;
+    }
+}
+
+
+void Window::Run()
+{
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Clear the screen.
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+
+    // Create Vertex Array Object (VAO).
+    GLuint vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+
+    // Load shaders from the GLSL sources.
+    GLuint programID = LoadShaders(
+            "../lesson 7 – model loading/VertexShader.glsl",
+            "../lesson 7 – model loading/FragmentShader.glsl"
+    );
+
+    // Get a handle for our "MVP" uniform variable.
+    GLint matrixID = glGetUniformLocation(programID, "MVP");
+
+    // Load a texture...
+    GLuint texture = loadDDS("../resources/cube_uvmap.dds");
+
+    // Get a handle for our "myTextureSampler" uniform
+    GLint textureID  = glGetUniformLocation(programID, "myTextureSampler");
+
+    // Read our .obj file
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec2> uvs;
+    std::vector<glm::vec3> normals;
+    bool res = loadOBJ("../resources/cube.obj", vertices, uvs, normals);
+
+    // Load it into a VBO
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
+    GLuint uvBuffer;
+    glGenBuffers(1, &uvBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+    glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+
+    // Enable depth test.
+    glEnable(GL_DEPTH_TEST);
+
+    // Accept fragment if it closer to the camera than the former one.
+    glDepthFunc(GL_LESS);
+
+    // Cull triangles which normal is not towards the camera.
+    glEnable(GL_CULL_FACE);
+
+    do
+    {
+        // Clear the screen.
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Use our shader.
+        glUseProgram(programID);
+
+        computeMatricesFromInputs(window);
+        glm::mat4 ProjectionMatrix = getProjectionMatrix();
+        glm::mat4 ViewMatrix = getViewMatrix();
+        glm::mat4 ModelMatrix = glm::mat4(1.0);
+        glm::mat4 mvpView = ProjectionMatrix * ViewMatrix * ModelMatrix;
+        // Send our transformation to the currently bound shader in the "MVP" uniform.
+        glUniformMatrix4fv(matrixID, 1, GL_FALSE, &mvpView[0][0]);
+
+        // Bind our texture in Texture Unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // Set our "myTextureSampler" sampler to user Texture Unit 0
+        glUniform1i(textureID, 0);
+
+        // 1st attribute buffer: vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        // 2nd attribute buffer: colors
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        // Draw the triangle.
+        glDrawArrays(GL_TRIANGLES, 0, CUBE_VERTICES);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    while (Input::IsKeyPressed(window, KEYBOARD_KEY::ESC) && glfwWindowShouldClose(window) == 0);
+
+    // Cleanup VBO and shader
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &uvBuffer);
+    glDeleteProgram(programID);
+    glDeleteTextures(1, &texture);
+    glDeleteVertexArrays(1, &vertexArrayID);
+
+    glfwTerminate();
+}
