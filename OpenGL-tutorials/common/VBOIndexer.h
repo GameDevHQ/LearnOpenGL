29c796f0eca1d09be7abd873dddb84db39970b68
Valeryi Savich <Relrin78@gmail.com>
2017-02-20 20:45:39 +0300

    Added solution to the Lesson 9

---
+++ OpenGL-tutorials/common/VBOIndexer.h
@@ -0,0 +1,16 @@
+#ifndef VBOINDEXER_H
+#define VBOINDEXER_H
+#include <vector>
+#include <glm/glm.hpp>
+
+void indexVBO(
+        std::vector<glm::vec3> & in_vertices,
+        std::vector<glm::vec2> & in_uvs,
+        std::vector<glm::vec3> & in_normals,
+
+        std::vector<unsigned short> & out_indices,
+        std::vector<glm::vec3> & out_vertices,
+        std::vector<glm::vec2> & out_uvs,
+        std::vector<glm::vec3> & out_normals
+);
+#endif
